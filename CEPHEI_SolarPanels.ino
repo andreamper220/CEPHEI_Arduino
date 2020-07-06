a9f1875ce1cb50ae41a355fa0889bf45df1abe89
Mrsanyor <64320563+Mrsanyor@users.noreply.github.com>
2020-07-06 20:29:39 +0300

    ff

--- CEPHEI_SolarPanels.ino
+++ CEPHEI_SolarPanels.ino
@@ -472,11 +472,14 @@ void sendValue(String value)
 {
   Serial.print("@OK REPLY ");
 <<<<<<< HEAD
+<<<<<<< HEAD
   Serial.println(value);
 }
 
 void sendBufferSize()
 {
+=======
+>>>>>>> parent of 97e05d8... fix: separate function for sending of buffer size
 =======
 >>>>>>> parent of 97e05d8... fix: separate function for sending of buffer size
   Serial.print(value);
